a90851d805f54a6ad30989666ced95aaaed06bf9
minsoo.lee <minsoo.lee@8byte.co.kr>
2022-08-29 15:00:01 +0900

    agent script 수정

--- dockerfiles/teamcity-agent-conf/start.sh
+++ dockerfiles/teamcity-agent-conf/start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+echo "serverUrl=${SERVER_URL}" >> ./TeamAgent/conf/buildAgent.properties
+echo "workDir=../work" >> ./TeamAgent/conf/buildAgent.properties
+echo "tempDir=../temp" >> ./TeamAgent/conf/buildAgent.properties
+
 ./TeamAgent/bin/agent.sh start && \
 tail -f /dev/null
