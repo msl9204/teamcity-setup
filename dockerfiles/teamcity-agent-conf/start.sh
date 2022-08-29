7886a0107e1f800dc8bffbe1d26faa3c27decd6b
minsoo.lee <minsoo.lee@8byte.co.kr>
2022-08-29 17:30:54 +0900

    start.sh 권한 추가

--- dockerfiles/teamcity-agent-conf/start.sh
+++ dockerfiles/teamcity-agent-conf/start.sh
@@ -4,5 +4,11 @@ echo "serverUrl=${SERVER_URL}" >> ./TeamAgent/conf/buildAgent.properties
 echo "workDir=../work" >> ./TeamAgent/conf/buildAgent.properties
 echo "tempDir=../temp" >> ./TeamAgent/conf/buildAgent.properties
 
+mkdir -p ./TeamAgent/work
+mkdir -p ./TeamAgent/temp
+
+chmod +x ./TeamAgent/work
+chmod +x ./TeamAgent/temp
+
 ./TeamAgent/bin/agent.sh start && \
 tail -f /dev/null
