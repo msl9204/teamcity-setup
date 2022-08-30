1f5c002e342892c832615b8c59fec0962a3a6534
minsoo.lee <minsoo.lee@8byte.co.kr>
2022-08-30 10:54:52 +0900

    수정

--- dockerfiles/teamcity-agent-conf/start.sh
+++ dockerfiles/teamcity-agent-conf/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "serverUrl=${SERVER_URL}" >> ./buildagent/conf/buildAgent.properties
-echo "workDir=../work" >> ./buildagent/conf/buildAgent.properties
-echo "tempDir=../temp" >> ./buildagent/conf/buildAgent.properties
+echo "serverUrl=${SERVER_URL}" >> /opt/buildagent/conf/buildAgent.properties
+echo "workDir=../work" >> /opt/buildagent/conf/buildAgent.properties
+echo "tempDir=../temp" >> /opt/buildagent/conf/buildAgent.properties
 
-./buildagent/bin/agent.sh start && \
+/opt/buildagent/bin/agent.sh start && \
 tail -f /dev/null
